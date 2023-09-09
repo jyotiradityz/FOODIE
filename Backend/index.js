f60f69361056df063d3381bb07fba548a580f79b
Jyotiraditya Patil <patiljyotiraditya123@gmail.com>
2023-09-09 19:42:27 +0530

    Mongo Connected

--- Backend/index.js
+++ Backend/index.js
@@ -6,7 +6,9 @@ connectDB();
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.use(express.json())
 
+app.use('/api',require("./Routes/CreateUser"))
 app.listen(port, () => {
   console.log(`Listening on our fav song ${port}`)
 })
