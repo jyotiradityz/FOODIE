a1b19c99ab6c971b760c4a884bfb41e0782f97d0
Jyotiraditya Patil <patiljyotiraditya123@gmail.com>
2023-10-02 17:32:12 +0530

    Basic Adj.

--- Frontend/src/Screens/Home.jsx
+++ Frontend/src/Screens/Home.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react'
 import Navbar from '../Components/Navbar/Navbar'
 import Footer from '../Components/Footer'
 import Card from '../Components/Card/Card'
-import Caraousel from '../Components/Caraousel'
 const Home = () => {
   const [foodCat, setFoodCat] = useState([]);
   const [foodItem, setFoodItem] = useState([]);
@@ -29,7 +28,33 @@ const Home = () => {
     <div className='bg'>
       <Navbar />
       <div>
-        <Caraousel />
+      <div id="carouselExampleFade" class="carousel slide carousel-fade justify-content-between" data-bs-ride="carousel" style={{objectFit:"contain !important"}}>
+            <div class="carousel-inner" id='caro'>
+                <div className='carousel-caption' style={{zIndex:"10"}}>
+                    <form className="d-flex">
+                        <input className="form-control me-2" type="search" placeholder="I want this Food" aria-label="Search"/>
+                            <button className="btn" style={{"background":"#222222"}} type="submit">Search</button>
+                    </form>
+                </div>
+                <div class="carousel-item active">
+                    <img src="https://www.teenaagnel.com/wp-content/uploads/2019/12/food-photography-in-dubai.jpg" class="d-block w-100" alt="..." />
+                </div>
+                <div class="carousel-item active">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg" class="d-block w-100" alt="..." />
+                </div>
+                <div class="carousel-item active">
+                    <img src="https://www.foodiesfeed.com/wp-content/uploads/2023/04/strawberry-milk-splash.jpg" class="d-block w-100" alt="..." />
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
       </div>
       <div className='container'>
         {
@@ -44,7 +69,7 @@ const Home = () => {
                 foodItem.filter((item) => item.CategoryName === data.CategoryName)
                   .map(f_list => {
                     return (
-                      <div key={f_list._id} className='col-12 col-md-6 col-lg-3'>
+                      <div key={f_list._id} className='col-md-12 col-md-6 col-lg-3 m-2'>
                         <Card 
                           foodName={f_list.name}
                           options={f_list.options[0]}
